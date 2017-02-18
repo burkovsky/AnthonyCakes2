762ab04ed3c2c8d80f7ecbe5f0d6dfdc31e33274
Anton Burkovsky <burkovsky.anton@gmail.com>
2017-02-18 22:10:13 +0300

    enable gzip

--- App/app.js
+++ App/app.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var compression = require('compression');
 var path = require('path');
 var favicon = require('serve-favicon');
 
 var app = express();
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(compression()); 
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
 
