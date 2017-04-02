e9d37e520f397d1d6ff7845f3f921dfc4fa595c5
Anton Burkovsky <burkovsky.anton@gmail.com>
2017-04-03 00:26:33 +0300

    cackle comments UI

--- src/app/reviews/cackle-comments/cackle-comments.component.ts
+++ src/app/reviews/cackle-comments/cackle-comments.component.ts
@@ -4,13 +4,14 @@ import { appConfig } from "../../shared/configs/app.config";
 
 @Component({
     selector: "ac-cackle-comments",
-    // styles: [String(require("./cackle-comments.component.scss"))],
+    styles: [String(require("./cackle-comments.component.scss"))],
     template: require("./cackle-comments.component.html"),
 })
 export default class CackleCommentsComponent implements OnInit {
     ngOnInit(): void {
         let widgets = (window as IWindowCackleWidget).cackle_widget;
         widgets.push({
+            guestHideEmail: true,
             id: appConfig.cackle.widgetId,
             widget: "Comment",
         });
