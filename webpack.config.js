c0fa2a4765a617e0d57f9eb81cabb19cd195efe9
Anton Burkovsky <burkovsky.anton@gmail.com>
2017-04-03 02:04:55 +0300

    webpack env variable fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 ﻿'use strict';
 
 module.exports = function(env) {
-    return require('./configs/webpack.config.' + env + '.js')({
-        env: env
-    });
+    return require('./configs/webpack.config.' + env + '.js')(env);
 };
